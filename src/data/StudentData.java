2a54ac86f3be606469ca58857c038ed6624b7dcd
JahanzaibTahir94 <Jahanzaib.tahir@khired.com>
2020-04-26 16:21:54 +0500

    Reduce + Filer + Map

--- src/data/StudentData.java
+++ src/data/StudentData.java
@@ -15,11 +15,11 @@ public class StudentData {
 
     public  static List<Student> getAllStudents()
     {
-        Student student = new Student(1,"Ahmed",3.4, Arrays.asList("Bike Riding","Swimming"));
-        Student student1 = new Student(2,"Bilal",3.0, Arrays.asList("Gaming","Pool","Reading"));
-        Student student2 = new Student(3,"Osama",3.9, Arrays.asList("Boxing","Driving"));
-        Student student3 = new Student(4,"John",3.5, Arrays.asList("Driving","Swimming","Badminton"));
-        Student student4 = new Student(1,"Adams",3.95, Arrays.asList("Bike Riding","Swimming","Movies"));
+        Student student = new Student(1,"Ahmed",3.4, Arrays.asList("Bike Riding","Swimming"),15);
+        Student student1 = new Student(2,"Bilal",3.0, Arrays.asList("Gaming","Pool","Reading"),5);
+        Student student2 = new Student(3,"Osama",3.9, Arrays.asList("Boxing","Driving"),7);
+        Student student3 = new Student(4,"John",3.5, Arrays.asList("Driving","Swimming","Badminton"),13);
+        Student student4 = new Student(1,"Adams",3.95, Arrays.asList("Bike Riding","Swimming","Movies"),3);
         List<Student> studentList = Arrays.asList(student,student1,student2,student3,student4);
 
         return studentList;
